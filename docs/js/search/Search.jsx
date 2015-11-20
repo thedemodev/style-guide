4e928d0c57ff0b082cbb6e4d561b70b4055af4aa
David Knezić <davidknezic@gmail.com>
2015-11-20 09:38:26 +0100

    Put all rendering into one place

--- docs/js/search/Search.jsx
+++ docs/js/search/Search.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 import Suggestion from './Suggestion';
 
@@ -8,179 +8,129 @@ const KEY_DOWN = 40
 
 export default class Search extends Component {
 
-    render() {
-        return (
-            <div className="search search--header autocomplete" >
-                <input
-                  className="autocomplete__input search__input search--header"
-                  type="text"
-                  defaultValue={this.props.searchTerm}
-                  onChange={this._onSearchTermChange.bind(this)}
-                  onKeyUp={this._onSearchInputKeyUp.bind(this)}
-                   />
-                {this._renderIcon()}
-                <div className="autocomplete__suggestions" >
-                    {this._renderError()}
-                    {this._renderLoading()}
-                    {this._renderSuggestions()}
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="search search--header autocomplete">
+        <input
+          className="autocomplete__input search__input search--header"
+          type="text"
+          defaultValue={this.props.searchTerm}
+          onChange={this._onSearchTermChange.bind(this)}
+          onKeyUp={this._onSearchInputKeyUp.bind(this)}/>
+        <svg className="search__icon search--header icon-svg">
+          <use xlinkHref="${this.props.icon}"/>
+        </svg>
+        <div className="autocomplete__suggestions">
+          {this.props.searchTerm && this.props.error ? (
+            <div className="docs-search__error">An error occurred :(</div>
+          ) : null}
+          {this.props.searchTerm && this.props.loading ? (
+            <div className="docs-search__loading">Loading...</div>
+          ) : null}
+          {this.props.suggestions ? (
+            this.props.suggestions.map((suggestion) => {
+              return (
+                <Suggestion
+                  key={suggestion.ref}
+                  isSelected={suggestion.ref == this.props.selectedSuggestion}
+                  title={suggestion.title}
+                  href={this.props.baseUrl + suggestion.link}
+                  tags={suggestion.tags}
+                  onMouseOver={this._onSuggestionMouseOver.bind(this, suggestion)}/>
+              )
+            })
+          ) : null}
+        </div>
+      </div>
+    )
+  }
 
-    _renderIcon() {
-      let html = {
-        __html: '<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="' +
-          this.props.icon +
-          '"></use>'
-      };
+  _onSearchTermChange(e) {
+    this.props.setSearchTerm(e.target.value);
+  }
 
-      return (
-        <svg
-          dangerouslySetInnerHTML={html}
-          className="search__icon search--header icon-svg" />
-      )
-    }
+  _onSearchInputKeyUp (e) {
+    switch(e.which) {
+      case KEY_UP:
+        this._selectPrevSuggestion();
+        break;
+      case KEY_DOWN:
+        this._selectNextSuggestion();
+        break;
+      case KEY_ENTER:
+        this._openCurrentOrFirstSuggestion();
+        break;}
+  }
 
-    _onSearchInputKeyUp(e) {
-      switch(e.which) {
-        case KEY_UP:
-          this._selectPrevSuggestion();
-          break;
-        case KEY_DOWN:
-          this._selectNextSuggestion();
-          break;
-        case KEY_ENTER:
-          this._openCurrentOrFirstSuggestion();
-          break;
-      }
-    }
+  _getCurrentSuggestion () {return this.props.suggestions.find((suggestion) => {
+      return suggestion.ref == this.props.selectedSuggestion;
+    });}
 
-    _getCurrentSuggestion() {
-      return this.props.suggestions.find((suggestion) => {
-        return suggestion.ref == this.props.selectedSuggestion;
-      })
-    }
+  _selectPrevSuggestion () {
+    let current = this._getCurrentSuggestion();
 
-    _selectPrevSuggestion() {
-      let current = this._getCurrentSuggestion();
+    let prev = null;
 
-      let prev = null;
+    if (current) {
+      let index = this.props.suggestions.indexOf(current);
 
-      if (current) {
-        let index = this.props.suggestions.indexOf(current);
-
-        if(index != 0) {
-          prev = this.props.suggestions[index-1];
-        }
-
-      }
-
-      if (!prev) {
-        prev = this.props.suggestions[this.props.suggestions.length - 1];
-      }
-
-      if (prev) {
-        this.props.setSelectedSuggestion(prev.ref);
+      if (index != 0) {
+        prev = this.props.suggestions[index - 1];
       }
 
     }
 
-    _selectNextSuggestion() {
-      let current = this._getCurrentSuggestion();
+    if (!prev) {
+      prev = this.props.suggestions[this.props.suggestions.length - 1];
+    }
 
-      let next = null;
+    if (prev) {this.props.setSelectedSuggestion(prev.ref);}
 
-      if (current) {
-        let index = this.props.suggestions.indexOf(current);
+  }
 
-        if(index+1 < this.props.suggestions.length) {
-          next = this.props.suggestions[index+1];
-        }
+  _selectNextSuggestion () {
+    let current = this._getCurrentSuggestion();
 
+    let next = null;
+
+    if (current) {
+      let index = this.props.suggestions.indexOf(current);
+
+      if (index + 1 < this.props.suggestions.length) {
+        next = this.props.suggestions[index + 1];
       }
 
-      if (!next) {
-        next = this.props.suggestions[0];
-      }
-
-      if (next) {
-        this.props.setSelectedSuggestion(next.ref);
-      }
-
     }
 
-    _openCurrentOrFirstSuggestion() {
-      let toOpen = this._getCurrentSuggestion();
-
-      if(!toOpen) {
-        toOpen = this.props.suggestions[0];
-      }
-
-      if(toOpen) {
-        window.location.href = toOpen.link;
-      }
+    if (!next) {
+      next = this.props.suggestions[0];
     }
 
-    _onSearchTermChange(e) {
-        this.props.setSearchTerm(e.target.value);
+    if (next) {this.props.setSelectedSuggestion(next.ref);}
+
+  }
+
+  _openCurrentOrFirstSuggestion () {
+    let toOpen = this._getCurrentSuggestion();
+
+    if (!toOpen) {
+      toOpen = this.props.suggestions[0];
     }
 
-    _renderSuggestions() {
-        if(this.props.suggestions) {
-            return this.props.suggestions.map((suggestion) => {
-                let isSelected = suggestion.ref == this.props.selectedSuggestion;
-
-                return (
-                    <Suggestion
-                      key={suggestion.ref}
-                      isSelected={isSelected}
-                      title={suggestion.title}
-                      href={this.props.baseUrl + suggestion.link}
-                      tags={suggestion.tags}
-                      onMouseOver={this._onSuggestionMouseOver.bind(this, suggestion)}
-                      />
-                )
-            });
-        }
-
-        return null;
+    if (toOpen) {
+      window.location.href = toOpen.link;
     }
+  }
 
-    _onSuggestionMouseOver(suggestion) {
-      this.props.setSelectedSuggestion(suggestion.ref);
-    }
-
-    _renderError() {
-        if(this.props.searchTerm && this.props.error) {
-            return (
-                <div className="docs-search__error" >An error occurred :(</div>
-            )
-        }
-
-        return null;
-    }
-
-    _renderLoading() {
-        if(this.props.searchTerm && this.props.loading) {
-            return (
-                <div className="docs-search__loading" >Loading...</div>
-            )
-        }
-
-        return null;
-    }
+  _onSuggestionMouseOver (suggestion) {this.props.setSelectedSuggestion(suggestion.ref);}
 }
 
 Search.propTypes = {
-    loading: React.PropTypes.bool,
-    error: React.PropTypes.oneOfType([
-      React.PropTypes.object,
-      React.PropTypes.bool,
-    ]),
-    suggestions: React.PropTypes.array,
-    selectedSuggestion: React.PropTypes.string,
-    setSelectedSuggestion: React.PropTypes.func.isRequired,
-    setSearchTerm: React.PropTypes.func.isRequired,
-    icon: React.PropTypes.string.isRequired,
+  loading: React.PropTypes.bool,
+  error: React.PropTypes.oneOfType([React.PropTypes.object, React.PropTypes.bool]),
+  suggestions: React.PropTypes.array,
+  selectedSuggestion: React.PropTypes.string,
+  setSelectedSuggestion: React.PropTypes.func.isRequired,
+  setSearchTerm: React.PropTypes.func.isRequired,
+  icon: React.PropTypes.string.isRequired
 };
