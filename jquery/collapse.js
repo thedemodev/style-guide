cae8fc1bbbce5ac3c413c0f20ae08220687ffeca
Gabriel Koch <koch_gabriel@hotmail.com>
2015-12-18 20:40:37 +0100

    Slight refactoring of the collapse plugin

--- jquery/collapse.js
+++ jquery/collapse.js
@@ -1,108 +1,107 @@
-(function ($, Bacon) {
-  class Collapse {
-    constructor(element, options) {
-      this.$element = $(element)
-      this.$triggers = this.$element.find('[data-trigger]')
-      this.$panels = this.$element.find('[data-panel]')
-      this.$panels.css('display', 'none').removeClass('is-hidden')
-      this.options = $.extend({}, { single: false, open: '' }, options)
-      this.options.open = this.options.open.split(' ')
-      this.init()
-    }
+import $ from 'jquery'
 
-    init() {
-      let clicks = this.$triggers
-        .asEventStream('click')
-        .doAction('.preventDefault')
-        .map((event) => this.mapEventToCommand(event))
+class Collapse {
+  constructor(element, options) {
+    this.$element = $(element)
+    this.$triggers = this.$element.find('[data-trigger]')
+    this.$panels = this.$element.find('[data-panel]')
+    this.$panels.css('display', 'none').removeClass('is-hidden')
+    this.options = $.extend({}, { single: false, open: '' }, options)
+    this.options.open = this.options.open.split(' ')
+    this.init()
+  }
 
-      clicks.onValue((event) => this.toggleTrigger(event))
-      clicks.onValue((event) => this.togglePanel(event))
+  init() {
+    let clicks = this.$triggers
+      .asEventStream('click')
+      .doAction('.preventDefault')
+      .map((event) => this.mapEventToCommand(event))
 
-      for (let o of this.options.open) {
-        let e = {
-          target: o
-        }
+    clicks.onValue((event) => this.toggleTrigger(event))
+    clicks.onValue((event) => this.togglePanel(event))
 
-        this.toggleTrigger(e)
-        this.togglePanel(e)
-      }
-    }
-
-    mapEventToCommand(event) {
-      let $e = $(event.target)
-
-      return {
-        target: $e.data('trigger')
-      }
-    }
-
-    scanCommands(opened, command) {
-      if (command.type === 'close') {
-        opened = opened.filter((target) => target !== command.target)
+    for (let o of this.options.open) {
+      let e = {
+        target: o
       }
 
-      if (command.type === 'open') {
-        if (this.options.single) opened = [command.target]
-        if (opened.indexOf(command.target) < 0) opened.push(command.target)
-      }
-      return opened
-    }
-
-    toggleTrigger(event) {
-      let triggers = this.$triggers.filter((index, e) => {
-        return $(e).data('trigger') === event.target
-      })
-
-      triggers.each((i, e) => {
-        console.log(e)
-        e.classList.toggle('is-active')
-      })
-    }
-
-    togglePanel(event) {
-      let panel = this.$panels.filter((index, e) => {
-        return e.dataset.panel === event.target
-      })
-      let $panel = $(panel)
-      if (!$panel.hasClass('is-open')) {
-        $panel.slideDown(250, function () {
-          $panel.addClass('is-open')
-        })
-      }
-      else {
-        $panel.slideUp(250, function () {
-          $panel.removeClass('is-open')
-        })
-      }
+      this.toggleTrigger(e)
+      this.togglePanel(e)
     }
   }
 
-  let Plugin = function (options) {
-    let params = arguments
+  mapEventToCommand(event) {
+    let $e = $(event.target)
 
-    return this.each(function () {
-      let $this = $(this)
-      let data = $this.data('aem.collapse')
+    return {
+      target: $e.data('trigger')
+    }
+  }
 
-      if (!data) {
-        data = new Collapse(this, options)
-        $this.data('aem.collapse', data)
-      }
+  scanCommands(opened, command) {
+    if (command.type === 'close') {
+      opened = opened.filter((target) => target !== command.target)
+    }
+
+    if (command.type === 'open') {
+      if (this.options.single) opened = [command.target]
+      if (opened.indexOf(command.target) < 0) opened.push(command.target)
+    }
+    return opened
+  }
+
+  toggleTrigger(event) {
+    let triggers = this.$triggers.filter((index, e) => {
+      return $(e).data('trigger') === event.target
+    })
+
+    triggers.each((i, e) => {
+      console.log(e)
+      e.classList.toggle('is-active')
     })
   }
 
-  $.fn.collapse = Plugin
-  $.fn.collapse.Constructor = Collapse
-
-  $(window).on('load', function () {
-    $('[data-collapse]').each(function () {
-      let $collapse = $(this)
-      let data = $collapse.data()
-      Plugin.call($collapse, data)
+  togglePanel(event) {
+    let panel = this.$panels.filter((index, e) => {
+      return e.dataset.panel === event.target
     })
+    let $panel = $(panel)
+    if (!$panel.hasClass('is-open')) {
+      $panel.slideDown(250, function () {
+        $panel.addClass('is-open')
+      })
+    }
+    else {
+      $panel.slideUp(250, function () {
+        $panel.removeClass('is-open')
+      })
+    }
+  }
+}
+
+let Plugin = function (options) {
+  let params = arguments
+
+  return this.each(function () {
+    let $this = $(this)
+    let data = $this.data('aem.collapse')
+
+    if (!data) {
+      data = new Collapse(this, options)
+      $this.data('aem.collapse', data)
+    }
   })
+}
 
-})(jQuery, Bacon)
+$.fn.collapse = Plugin
+$.fn.collapse.Constructor = Collapse
+
+$(window).on('load', function () {
+  $('[data-collapse]').each(function () {
+    let $collapse = $(this)
+    let data = $collapse.data()
+    Plugin.call($collapse, data)
+  })
+})
 
 // Copyright AXA Versicherungen AG 2015
