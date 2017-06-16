4dc3749f0df3b1cbc0a175058529a516a7e0f6a1
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2017-06-16 09:22:15 +0200

    added quick sort hack to postcss build

--- webpack/custom-postcss.js
+++ webpack/custom-postcss.js
@@ -1,13 +1,16 @@
+require('babel-register')
 var postcss = require('postcss')
 var autoprefixer = require('autoprefixer')
 var cssmqpacker = require('css-mqpacker')
 var csswring = require('csswring')
 var pseudoelements = require('postcss-pseudoelements')
+var fixIeTargetOrder = require('../lib/postcss-fix-ie-target-order').default
 
 var processor = postcss([
   pseudoelements,
   autoprefixer,
   cssmqpacker({ sort: true }),
+  fixIeTargetOrder(),
   csswring,
 ])
 
